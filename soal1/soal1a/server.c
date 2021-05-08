2de4b57825bde7fdd52d022c0338521a10e65942
drigoalexander <73428164+drigoalexander@users.noreply.github.com>
2021-05-08 21:49:50 +0700

    Create server.c

---
+++ soal1/soal1a/server.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    
+      
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+      
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+void *client (void *arg){
+int socket =* (int *)arg; 
+char input[10];
+FILE *dir;
+while(1)
+    {
+dir = fopen("akun.txt","a+")
+read(socket, input, 10);
+if(strcmp(input,"Login")==0)
+{
+read(socket,buffer,1024);
+int temp[200];
+
+if(fgets(temp,200,dir) !=NULL){
+char output[]= "Welcome";
+send(socket,output,strlen(output),0);
+break
+}
+else{
+char output[] = "Login Failed";
+send(socket,output,strlen(output),0);
+}
+}
+}
