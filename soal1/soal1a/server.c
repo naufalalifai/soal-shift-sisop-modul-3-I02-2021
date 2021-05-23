96ee9cf01b25cb53e8379558209f3252210e4233
drigoalexander <73428164+drigoalexander@users.noreply.github.com>
2021-05-23 21:09:47 +0700

    Update server.c

--- soal1/soal1a/server.c
+++ soal1/soal1a/server.c
@@ -4,15 +4,195 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
-#define PORT 8080
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+#include <pthread.h>
+#include <ctype.h> 
+#include <limits.h>
+#define PORT 8080  
+#define _OPEN_THREADS
+
+pthread_t tid[1000];
+
+void clear_buffer(char* b) 
+{
+    int i;
+    for (i = 0; i < BUFSIZ; i++)
+        b[i] = '\0';
+}
+
+void pisah(char str[], char fileex[], char pemisah, int *idx)
+{
+    int index_ex = 0;
+    while(*idx < strlen(str) && str[*idx] != pemisah) {
+        fileex[index_ex] = str[*idx];
+        *idx += 1;
+        index_ex++;    
+    }
+    fileex[index_ex] = '\0';
+    *idx += 1;
+}
+
+void ekstrak(char buffers[], char filename[], char pub[], char tahun_pub[])
+{
+    int idx = 0;
+    int len = strlen(buffers);
+
+    pisah(buffers, filename, ':', &idx);
+    pisah(buffers, pub , ':', &idx);
+    pisah(buffers, tahun_pub, ':', &idx);
+}
+
+
+void upload(int sockets, char namafile[])
+{
+    printf("Upload Proses");
+    char buffer[BUFSIZ];
+    int valread;
+    ssize_t length;
+
+    send(sockets,"Upload", strlen("Upload"),0);
+    clear_buffer(buffer);
+    read(sockets,buffer,BUFSIZ);9
+
+    FILE *filedir;
+    char file_path[100];
+    sprintf(file_path, "FILES/%s", namafile);
+
+    filedir = fopen(file_path, "w");
+
+    
+    clear_buffer(buffer);
+    read(sockets, buffer, BUFSIZ);
+    fprintf(filedir, "%s", buffer);
+
+    
+    fclose(filedir);    
+}
+
+void *client(void *arg)
+{
+    int socketfd = *(int *)arg;
+    int valread;
+    char command[BUFSIZ];
+    char userpass[100];
+
+    clear_buffer(command);
+    valread = read(socketfd , command, BUFSIZ);
+    fprintf(stdout, "Receive connection handshake %s\n", command);
+
+    send(socketfd, "SUCCESS", strlen("SUCCESS"), 0);
+
+    clear_buffer(command);
+    valread = read(socketfd , command, BUFSIZ);
+
+    while(1)
+    {    
+        // printf("Ini ngeprint while client\n");
+        FILE *fdir;
+        fdir = fopen("akun.txt","a+");    
+        clear_buffer(command);
+        valread = read(socketfd, command, BUFSIZ);
+        // printf("%s-%ld",command,strlen(command));
+        if(strcmp(command,"register")==0)
+        { 
+            char buffers[BUFSIZ];
+            clear_buffer(buffers);
+            valread = read(socketfd,buffers,BUFSIZ);
+            // printf("%s\n", buffers);
+            printf("Registrasi Sukses\n"); 
+            fprintf(fdir,"%s\n",buffers);
+            //pesan yang ditampilkan ke client
+            send(socketfd,"Register Success",strlen("Register Success"),0);
+        }
+        else if (strcmp(command,"login")==0)
+        {
+            char temp[BUFSIZ];
+            char userada[10];
+            // printf("Ini kalo dia login\n");  
+            char buffers[BUFSIZ];
+            int flag = 0;
+            clear_buffer(buffers);
+            valread = read(socketfd,buffers,BUFSIZ);
+            strcpy(userpass,buffers);
+            while(fgets(temp, BUFSIZ, fdir) != NULL) 
+            {
+                // printf("%s",temp);
+                if((strstr(temp, buffers)) != NULL) 
+                {
+                    printf("Login Berhasil\n");
+                    flag = 1;
+                    char kata[20] = "Login Success";
+                    send(socketfd,kata, strlen(kata),0);
+                    break;
+                }                
+            }
+
+            if(flag == 0)
+            {
+                printf("Login Gagal\n");
+                char kata[20] = "Login Failed";
+                send(socketfd,kata, strlen(kata),0);
+            }
+        }
+        else if(strcmp(command,"add")==0)
+        {
+            char buffers[BUFSIZ];
+            printf("Adding to Database :\n");
+
+            send(socketfd, "success", strlen("success"), 0);
+            FILE *fdirc;
+            fdirc = fopen("file.tsv", "a+");
+            if (fdirc == NULL) 
+            {
+                perror("No File");
+                exit(EXIT_FAILURE);
+            }
+            clear_buffer(buffers);
+            valread = read(socketfd, buffers, BUFSIZ);
+
+            char filename[50];
+            char pub[50];
+            char tahun_pub[10];            
+
+            ekstrak(buffers, filename, pub, tahun_pub);
+
+            char data[200];
+            sprintf(data, "FILES/%s\t%s\t%s", filename, pub, tahun_pub);
+
+            upload(socketfd,filename);
+
+
+            send(socketfd, "complete", strlen("complete"), 0);
+
+            //fprintf(fdirc, "%s\n", buffers);
+            fprintf(fdirc, "%s\n", data);
+
+            FILE* log = fopen("running.log", "a") ;
+            fprintf(log, "Tambah : %s (%s)\n", filename, userpass);
+            fclose(log) ;
+            
+            fclose(fdirc);
+
+        }
+        
+        fclose(fdir);
+
+        if(valread == 0)
+        {
+            return 0;
+        }
+    }
+}
 
 int main(int argc, char const *argv[]) {
     int server_fd, new_socket, valread;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    
+    char buffer[BUFSIZ] = {0};
+    char *hello = "Hello from server";
       
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
@@ -38,31 +218,20 @@ int main(int argc, char const *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-void *client (void *arg){
-int socket =* (int *)arg; 
-char input[10];
-FILE *dir;
-while(1)
-    {
-dir = fopen("akun.txt","a+")
-read(socket, input, 10);
-if(strcmp(input,"Login")==0)
-{
-read(socket,buffer,1024);
-int temp[200];
+    int total = 0;
 
-if(fgets(temp,200,dir) !=NULL){
-char output[]= "Welcome";
-send(socket,output,strlen(output),0);
-break
-}
-else{
-char output[] = "Login Failed";
-send(socket,output,strlen(output),0);
-}
-}
+    while(1)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_create(&(tid[total]), NULL, &client, &new_socket);
+        pthread_join(tid[total],NULL);
+        total = total+1;   
+    }  
+
+    return 0;
 }
